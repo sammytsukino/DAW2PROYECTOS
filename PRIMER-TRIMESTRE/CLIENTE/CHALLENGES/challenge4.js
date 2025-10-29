9ae6f21c69ed04bcd795deab6c8560cbeb5f5093
sammytsukino <samy.cabello.g@gmail.com>
2025-10-29 11:05:26 +0100

    challenge 4

---
+++ PRIMER-TRIMESTRE/CLIENTE/CHALLENGES/challenge4.js
@@ -0,0 +1,39 @@
+const tree = {
+  value: '🎁',
+  left: {
+    value: '🎄',
+    left: {
+      value: '⭐',
+      left: null,
+      right: null
+    },
+    right: {
+      value: '🎅',
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    value: '❄️',
+    left: null,
+    right: {
+      value: '🦌',
+      left: null,
+      right: null
+    }
+  }
+}
+
+
+
+function treeHeight(tree) {
+    if(tree) { const leftHeight = treeHeight(tree.left);
+    const rightHeight = treeHeight(tree.right);
+    return 1 + Math.max(leftHeight, rightHeight);
+        
+    } else {
+        return 0;
+    }
+}
+
+console.log(treeHeight(tree))
