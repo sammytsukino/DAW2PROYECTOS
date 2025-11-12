3f52fa3fa8d5071e7cd4587997d07a5c0d9d6adf
sammytsukino <samy.cabello.g@gmail.com>
2025-11-12 13:22:26 +0100

    Refactor cinema app templates and views: implement cookie-based theme selection, add movie edit and delete functionality, and enhance layout structure.

--- PRIMER-TRIMESTRE/SERVIDOR/dwes-ud03-ejercicio2-main/cinemaapp/urls.py
+++ PRIMER-TRIMESTRE/SERVIDOR/dwes-ud03-ejercicio2-main/cinemaapp/urls.py
@@ -4,9 +4,14 @@ from django.urls import path
 from cinemaapp import views
 
 urlpatterns = [
-    path('form', views.MovieCreate.as_view(), name='form'),
-    path('list', views.MovieList.as_view(), name='movie_list'),
-    path('<int:pk>/edit', views.MovieUpdate.as_view(), name='movie_edit'),
-    path('<int:pk>/delete', views.MovieDelete.as_view(), name='movie_delete'),
-    path('<int:pk>/detail', views.MovieDetail.as_view(), name='movie_detail')
+    path('form', views.form, name='form'),
+    path('list', views.movie_list, name='movie_list'),
+    path('<int:pk>/edit', views.movie_edit, name='movie_edit'),
+    path('<int:pk>/delete', views.movie_delete, name='movie_delete'),
+    path('<int:pk>/detail', views.MovieDetail.as_view(), name='movie_detail'),
+    path('temaoscuro', views.temaoscuro, name='temaoscuro'),
+    path('temaamarillo', views.temaamarillo, name='temaamarillo'),
+    path('borracookie', views.borracookie, name='borracookie')
 ]
+
+
--- PRIMER-TRIMESTRE/SERVIDOR/dwes-ud03-ejercicio2-main/cinemaapp/views.py
+++ PRIMER-TRIMESTRE/SERVIDOR/dwes-ud03-ejercicio2-main/cinemaapp/views.py
@@ -18,6 +18,30 @@ def form(request):
         form = MovieForm()
     return render(request, 'cinemaapp/form.html', {'form': form})
 
+def movie_edit(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == 'POST':
+        form = MovieForm(request.POST, request.FILES, instance=movie)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Hecho")
+    else:
+        form = MovieForm(instance=movie)
+    return render(request, "cinemaapp/form.html", {"form": form})
+
+
+def movie_list(request):
+    tema = request.COOKIES.get('tema', 'nada')
+    movies = Movie.objects.all()
+    return render(request, 'cinemaapp/list.html', {'movies': movies, 'tema': tema})
+
+def movie_delete(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == 'POST':
+        movie.delete()
+        return redirect('movie_list')
+    return render(request, 'cinemaapp/confirm_delete.html', {})
+
 class MovieCreate(CreateView):
     model = Movie
     form_class = MovieForm
@@ -44,3 +68,27 @@ class MovieDetail(DetailView):
     model = Movie
     template_name = 'cinemaapp/detail.html'
     context_object_name = 'movie'
+
+def crea_cookie(request):
+    resp = redirect('movie_list')
+    resp.set_cookie('tema', 'oscuro')
+    return resp
+
+def lee_cookie(request):
+    tema = request.COOKIES.get('tema', 'nada')
+    return HttpResponse(f'Tema actual: {tema}')
+
+def temaoscuro(request):
+    resp = redirect('form')
+    resp.set_cookie('tema', 'oscuro')
+    return resp
+
+def temaamarillo(request):
+    resp = redirect('form')
+    resp.set_cookie('tema', 'amarillo')
+    return resp
+
+def borracookie(request):
+    resp = redirect('form')
+    resp.delete_cookie('tema')
+    return resp
